2d0510ee6a5c0ccf41f9067f90334044f14cb13a
geektcp <geektcp@163.com>
2023-01-07 21:23:13 +0800

    caluate duration

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 git pull
-date && mvn clean deploy -P release,gpg && date
+start_time=`date + '%Y-%m-%d %H:%M:%S'`
+
+mvn clean deploy -P release,gpg
+
+end_time=`date + '%Y-%m-%d %H:%M:%S'`
+sc=$(date --date="${start_time}" +%s)
+ec=$(date --date="${end_time}" +%s)
+
+delta=ec-sc
+duration_minute=$((delta/60))
+duration_second=$((delta%60))
+echo "duration: ${duration_minute} minute ${duration_second} second"
